8d248d9b1082cea055149f775304ba5c8ada4b8b
Firdaus Ishaddin <firdausishaddin@gmail.com>
2020-02-26 07:52:54 +0800

    remove unnecessary route

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['namespace' => 'Rekamy\Generator\Http\Controllers'], function () {
-    Route::get('generator', 'ApiGeneratorController@index');
-    Route::get('logout', 'ApiGeneratorController@index');
-});
+// Route::group(['namespace' => 'Rekamy\Generator\Http\Controllers'], function () {
+//     Route::get('generator', 'ApiGeneratorController@index');
+//     Route::get('logout', 'ApiGeneratorController@index');
+// });
