f1c3b24784fe92a8b6eaeecc2b45f27484404fa4
Zahrin Azam <kidzenfxc@gmail.com>
2021-02-16 00:42:27 +0800

    remove type casting

--- src/templates/frontend/crud-flat/CrudEditTs.blade.php
+++ src/templates/frontend/crud-flat/CrudEditTs.blade.php
@@ -9,12 +9,13 @@ export default class Edit{$studly}Page extends Vue {
     $camel = new $studly
 
     async created() {
-        this.{$camel}.id = +this.\$route.params.id
+        this.{$camel}.id = this.\$route.params.id
         this.{$camel} = await this.{$camel}Bloc.get{$studly}(this.{$camel}.id);
     }
 
     async save() {
         try {
+            // FIXME: method spoofing should overide from base api
             this.{$camel}._method = \"PUT\";
             this.$camel = await this.{$camel}Bloc.update{$studly}(this.{$camel});
             widget.alertSuccess('Good Job!', 'You have successfully edit this $title');
