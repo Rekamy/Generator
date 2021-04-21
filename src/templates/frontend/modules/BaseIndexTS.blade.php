4226f70873d4b80dd678797f9ebdd0d485075fc2
Razlan Razak <razlanrzk@gmail.com>
2021-04-22 05:14:56 +0800

    api and base index modules

--- src/templates/frontend/modules/BaseIndexTS.blade.php
+++ src/templates/frontend/modules/BaseIndexTS.blade.php
@@ -1,8 +1,15 @@
 <?php
-echo" export * from './auth';\n";
+echo" 
+export * from './auth';\n
+export * from './user';\n
+export * from './role';\n
+export * from './permission';\n
+export * from './reference';\n
+";
 foreach ($modules as $module) :
     echo "export * from './$module';\n";
 endforeach;
+
 ?>
 <?= "
 export default {
--- src/templates/frontend/modules/apiTS.blade.php
+++ src/templates/frontend/modules/apiTS.blade.php
@@ -1,17 +1,13 @@
 <?= "
-import { api } from '@/core/services/api';
-import { {$studly} } from './index';
 import CrudApi from '@/core/abstracts/crud-api';
 
-
 export class {$studly}Api extends CrudApi {
-    private BASE_URL: string;
-
     
     constructor() {
         super();
         this.BASE_URL = \"/{$slug}\";
     }
+
 }
 
 const {$camel}Api = new {$studly}Api();
