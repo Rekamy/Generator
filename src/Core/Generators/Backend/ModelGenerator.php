5058ac405bf669ef5213284983e7cf55255c77c7
Zahrin Azam <kidzenfxc@gmail.com>
2021-05-25 07:02:16 +0800

    bugfix

--- src/Core/Generators/Backend/ModelGenerator.php
+++ src/Core/Generators/Backend/ModelGenerator.php
@@ -33,9 +33,6 @@ class ModelGenerator
         $this->relations = collect();
 
         $this->context->info("Make relationships collection...");
-        if ($this->context->options->get('relation')) {
-            $this->makeRelations();
-        }
     }
 
     public function generate()
@@ -47,7 +44,8 @@ class ModelGenerator
                 $data['table'] = $table;
 
                 $data['columns'] = collect($this->context->db->listTableColumns($table))->except('id');
-                $data['uuid'] = collect($this->context->db->listTableColumns($table))->get('id')->gettype()->getname();
+                $data['uuid'] = collect($this->context->db->listTableColumns($table))->get('id') ?
+                    collect($this->context->db->listTableColumns($table))->get('id')->getType()->getName() : false;
                 $data['isUuid'] = ($data['uuid'] == "string");
                 $data['softDelete'] = $data['columns']->get('deleted_at') && $this->context->options->get('softDelete');
                 $data['className'] = Str::of($table)->singular()->studly();
@@ -75,92 +73,5 @@ class ModelGenerator
         } catch (\Throwable $th) {
             throw $th;
         }
-    }
-
-    public function makeRelations()
-    {
-        $this->relations = collect();
-        foreach ($this->tables as $table) {
-            $this->tempRelations = [];
-            collect($this->context->db->listTableForeignKeys($table))
-                ->each(function ($fk) use ($table) {
-                    $relation = $this->makeRelation($fk);
-                    if (empty($this->tempRelations[$table])) {
-                        $this->tempRelations[$table] = [];
-                    }
-                    array_push($this->tempRelations[$table], $relation);
-
-                    $foreignTable = $fk->getForeignTableName();
-                    $inverseRelation = $this->makeInverseRelation($fk, $table);
-                    if (empty($this->tempRelations[$foreignTable])) {
-                        $this->tempRelations[$foreignTable] = [];
-                    }
-                    array_push($this->tempRelations[$foreignTable], $inverseRelation);
-                });
-            $this->relations->push($this->tempRelations);
-        }
-        $collect = collect();
-        $this->relations->filter(function ($item) {
-            return !empty($item);
-        })
-            ->each(function ($item)  use ($collect) {
-                collect($item)->each(function ($item, $key) use ($collect) {
-                    $collect->put($key, $item);
-                });
-            });
-        $this->relations = $collect;
-    }
-
-    public function makeInverseRelation($details, $localTable)
-    {
-        $fkIsPrimary = in_array('id', $details->getForeignColumns());
-        if ($fkIsPrimary) {
-            $relationName = Str::plural(Str::camel($localTable));
-        } else {
-            $localKey = Str::studly($details->getForeignColumns());
-            $foreignTable = Str::camel($localTable);
-            $relationName = Str::plural($foreignTable . $localKey);
-            if (Str::contains($details->getForeignColumns(), $localTable)) {
-                str_replace($localTable, '', $details->getLocalColumns());
-            }
-        }
-
-        $foreignModel = Str::singular(Str::studly($localTable)) . '::class';
-
-        $html = "\tpublic function $relationName()\n";
-        $html .= "\t{\n";
-        $html .= "\t\treturn \$this->hasMany($foreignModel);\n";
-        $html .= "\t}\n";
-
-
-        // dd($details->getName());
-        return  $html;
-    }
-
-    public function makeRelation($details)
-    {
-
-        $fkIsPrimary = in_array('id', $details->getForeignColumns());
-        if ($fkIsPrimary) {
-            $relationName = Str::singular(Str::camel($details->getForeignTableName()));
-        } else {
-            $localKey = Str::studly($details->getLocalColumns());
-            $foreignTable = Str::camel($details->getForeignTableName());
-            $relationName = $foreignTable . $localKey;
-            if (Str::contains($details->getLocalColumns(), $details->getForeignTableName())) {
-                str_replace($details->getForeignTableName(), '', $details->getLocalColumns());
-            }
-        }
-
-        $foreignModel = Str::singular(Str::studly($details->getForeignTableName())) . '::class';
-
-        $html = "\tpublic function $relationName()\n";
-        $html .= "\t{\n";
-        $html .= "\t\treturn \$this->belongsTo($foreignModel);\n";
-        $html .= "\t}\n";
-
-
-        // dd($details->getName());
-        return  $html;
     }
 }
