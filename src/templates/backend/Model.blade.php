8a9ab8f5709281d60bc5f139721679987d44341c
Zahrin Azam <kidzenfxc@gmail.com>
2021-08-01 05:00:21 +0800

    bugfix

--- src/templates/backend/Model.blade.php
+++ src/templates/backend/Model.blade.php
@@ -73,21 +73,6 @@ class $className extends Model
 <?= "
     }
 
-    public function author()
-    {
-        return \$this->belongsTo(User::class, 'created_by');
-    }
-
-    public function editor()
-    {
-        return \$this->belongsTo(User::class, 'updated_by');
-    }
-
-    public function deletedBy()
-    {
-        return \$this->belongsTo(User::class, 'deleted_by');
-    }
-
 " 
 ?>
 <?php
--- src/templates/backend/UserModel.blade.php
+++ src/templates/backend/UserModel.blade.php
@@ -42,5 +42,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
 }
 STUB;
