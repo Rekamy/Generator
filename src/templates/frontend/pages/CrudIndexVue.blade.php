6130d16463f3ea3bb031e56a490da709495c194b
Acethegreat <razlanrzk@gmail.com>
2022-09-28 18:21:16 +0800

    v6 new structure

---
+++ src/templates/frontend/pages/CrudIndexVue.blade.php
@@ -0,0 +1,12 @@
+<?=
+"
+<template>
+  <BaseCard title='Crud Example'>
+  <CreateFormComponent v-model:example='example' @create='create' />
+  </BaseCard>
+</template>
+
+<script scoped lang=\"ts\" src=\"./index.ts\"></script>
+
+"
+?>
