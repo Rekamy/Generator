d440f470eea3dd4ea0ad496824c6e01fb42ea917
Zahrin Azam <kidzenfxc@gmail.com>
2023-01-25 13:41:43 +0800

    wip

--- src/templates/frontend/modules-vite/blocTSVite.blade.php
+++ src/templates/frontend/modules-vite/blocTSVite.blade.php
@@ -1,24 +1,26 @@
 <?=
 "
+import type { ${studly} } from "./model";
+
 export function use${studly}Bloc() {
     async function get${studlyPlural}(query?: object) {
-        return await crudApi<typeof ${studly}>(\"${slug}\").all(query);
+        return await crudApi<${studly}>(\"${slug}\").all(query);
     }
 
     async function get${studly}(id: ID, query?: object) {
-        return await crudApi<typeof ${studly}>(\"${slug}\").find(id, query);
+        return await crudApi<${studly}>(\"${slug}\").find(id, query);
     }
 
-    async function create${studly}(data: typeof ${studly}) {
-        return await crudApi<typeof ${studly}>(\"${slug}\").create(data);
+    async function create${studly}(data: ${studly}) {
+        return await crudApi<${studly}>(\"${slug}\").create(data);
     }
 
     async function update${studly}(id: ID, data: any) {
-        return await crudApi<typeof ${studly}>(\"${slug}\").update(id, data);
+        return await crudApi<${studly}>(\"${slug}\").update(id, data);
     }
 
     async function delete${studly}(id: ID) {
-        return await crudApi<typeof ${studly}>(\"${slug}\").destroy(id);
+        return await crudApi<${studly}>(\"${slug}\").destroy(id);
     }
 
     return {
